b826d12d2ce89c05eebb7d39b8527e43be273b73
Jeremy Holland <jeremy@jeremypholland.com>
2011-10-04 14:40:50 -0500

    initial import

---
+++ lib/kdtree/hyper_rect.rb
@@ -0,0 +1,43 @@
+class KDTree
+  class HyperRect
+    attr_accessor :minima, :maxima
+    def initialize(k)
+      @minima = []
+      @maxima = []
+      k.times do |i|
+        @minima << -Float::INFINITY
+        @maxima << Float::INFINITY
+      end
+    end
+
+    def clone
+      cl = self.class.new @minima.length
+      cl.minima = @minima.clone
+      cl.maxima = @maxima.clone
+      cl
+    end
+
+    def cut(dim, pivot)
+      left_hr = self.clone
+      left_hr.maxima[dim] = pivot
+      right_hr = self.clone
+      right_hr.minima[dim] = pivot
+      [left_hr, right_hr]
+    end
+
+    def intersects?(target, radius, metric)
+      near_point = KDPoint.new
+      target.length.times do |i|
+        if target[i] <= @minima[i]
+          coord = @minima[i]
+        elsif @minima[i] < target[i] && target[i] < @maxima[i]
+          coord = target[i]
+        else
+          coord = @maxima[i]
+        end
+        near_point[i] = coord
+      end
+      near_point.distance_from(target, metric) <= radius
+    end
+  end
+end
--- spec/kdtree_spec.rb
+++ spec/kdtree_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Kdtree" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe KDTree do
+  it "should exist" do
+    Module.constants.should include :KDTree
+    KDTree.should be_a Module
   end
 end
