2284a67ed65e9b610982e4cc1f8c22191af02385
Jeremy Holland <jeremy@jeremypholland.com>
2011-10-04 14:50:18 -0500

    Updating to avoid conflict with existing kdtree gem

--- spec/kdtree_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-describe KDTree do
-  it "should exist" do
-    Module.constants.should include :KDTree
-    KDTree.should be_a Module
-  end
-end
